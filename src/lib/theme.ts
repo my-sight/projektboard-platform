3cfbbd22aabf4b7746f024c569bbb11a01768fb8
my-sight <31878737+my-sight@users.noreply.github.com>
2025-09-01 15:20:50 +0200

    Update theme.ts

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,1 +1,9 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: { main: '#6366f1' },
+    secondary: { main: '#f59e42' },
+  },
+})
 
