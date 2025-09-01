fbdf8da3e5785176b1cd1834bccbbfe32ed23514
my-sight <31878737+my-sight@users.noreply.github.com>
2025-09-01 15:20:25 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Projektboard Platform</h1>
+      <p>Willkommen! Bitte einloggen oder Projekt erstellen.</p>
+    </main>
+  )
+}
 
