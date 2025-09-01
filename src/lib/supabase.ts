76e55ff71c13879a7c2cbce8092e4b649bcf3d4a
my-sight <31878737+my-sight@users.noreply.github.com>
2025-09-01 15:19:32 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,1 +1,17 @@
+import { createClientComponentClient, createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@supabase/supabase-js'
+import { cookies } from 'next/headers'
+
+export const createSupabaseClient = () => createClientComponentClient()
+export const createSupabaseServerClient = () => {
+  const cookieStore = cookies()
+  return createServerComponentClient({ cookies: () => cookieStore })
+}
+export const createSupabaseAdminClient = () => (
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    { auth: { autoRefreshToken: false, persistSession: false } }
+  )
+)
 
